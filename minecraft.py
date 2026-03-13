f4e9ef6213c077d67e71fc5e1e98d6f1eaec42df
Sheldon1436 <bh547516@georgetownisd.org>
2026-03-13 11:16:06 -0500

    added intro

--- minecraft.py
+++ minecraft.py
@@ -1,3 +1,4 @@
+from pydoc import text
 import tkinter as tk
 import random as ran
 
@@ -19,6 +20,16 @@ root = tk.Tk()
 root.title("Minecraft")
 root.geometry('10000x100000')
 root.state('zoomed')
+
+intro =  tk.Label(root, text="How to Play:\nYou must start by mining a stone or netherrack block.\nYou can only mine blocks next to blocks you've already mined.\nYou lose score for mining stone, deepslate, and netherrack.\nYour score is shown on the bottom left bedrock,\nand you can go to the next round by clicking 'Next'.\nIn between rounds, you can buy upgrades by spending your score.\nThese upgrades can boost ore spawns, the amount of score you get per ore,\nor remove the score penalties when mining netherrack, stone, and deepslate.\n\nOre Values:\nStone & Netherrack = -1\nDeepslate = -1.5\nCoal, Copper, & Nether Gold = 1.75\nRedstone & Lapis = 2.5\nIron, Gold, & Quartz = 3.25\nDiamond = 5\nEmerald & Netherite = 12.5")
+startB =  tk.Button(root, text = 'Start', bg='gray85', command= lambda: startGame())
+
+def startGame():
+    global intro, startB
+    intro.pack_forget()
+    startB.pack_forget()
+    nextRound()
+
 
 images = {'stone': tk.PhotoImage(file='assets/images/stoneImageMinecraft.png'),
             'deepslate': tk.PhotoImage(file='assets/images/deepslateImageMinecraft.png'),
@@ -369,10 +380,7 @@ def nextRound():
             blocks[r].append('barrier')
 
 
-
-
-
-
-nextRound()
+intro.pack(pady=150)
+startB.pack(pady=50)
 
 root.mainloop()
