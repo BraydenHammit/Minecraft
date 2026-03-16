6406d837ce26061bb888e5b0e4ad418e48a5399b
Sheldon1436 <bh547516@georgetownisd.org>
2026-03-16 12:01:48 -0500

    fixed embedded if to elifs

--- extra_code/oreFunct.py
+++ extra_code/oreFunct.py
@@ -8,25 +8,26 @@ def oreO(upgradeInv):
     else:
         randomNum = ran.randint(0,75)
 
-
-    if (randomNum == 32 or randomNum == 31) and upgradeInv['ore ext']:
-        ore = 'amethyst'
+    if randomNum == 0:
+        ore = 'emerald'
+    elif randomNum <= 1:
+        ore = 'diamond'
+    elif randomNum <= 4:
+        ore = 'gold'
+    elif randomNum <= 9:
+        ore = 'iron'
+    elif randomNum <= 13:
+        ore = 'lapis'
+    elif randomNum <= 17:
+        ore = 'redstone'
+    elif randomNum <= 23:
+        ore = 'coal'
     elif randomNum <= 30:
         ore = 'copper'
-        if randomNum <= 23:
-            ore = 'coal'
-            if randomNum <= 17:
-                ore = 'redstone'
-                if randomNum <= 13:
-                    ore = 'lapis'
-                    if randomNum <= 9:
-                        ore = 'iron'
-                        if randomNum <= 4:
-                            ore = 'gold'
-                            if randomNum <= 1:
-                                ore = 'diamond'
-                                if randomNum == 0:
-                                    ore = 'emerald'
+    elif (randomNum == 32 or randomNum == 31) and upgradeInv['ore ext']:
+        ore = 'amethyst'   
+    elif randomNum == 33 and upgradeInv['potato'] and ran.randrange(77) == 33:
+        ore = 'poisonous potato'
     else:
         ore = 'none'
 
@@ -42,14 +43,14 @@ def oreN(upgradeInv):
     else:
         randomNum = ran.randint(0,65)
 
-    if (randomNum == 27 or randomNum == 26) and upgradeInv['ore ext']:
-            ore = 'gilded blackstone'
+    if randomNum == 0:
+        ore = 'netherite'
+    elif randomNum <= 10:
+        ore = 'quartz'
     elif randomNum <= 25:
         ore = 'nether gold'
-        if randomNum <= 10:
-            ore = 'quartz'
-            if randomNum == 0:
-                ore = 'netherite'
+    elif (randomNum == 27 or randomNum == 26) and upgradeInv['ore ext']:
+        ore = 'gilded blackstone'
     else:
         ore = 'none'
 
