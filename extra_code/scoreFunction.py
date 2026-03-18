b1e9aacfed856a6720be1813f0a465ee06196d7d
Sheldon1436 <bh547516@georgetownisd.org>
2026-03-18 12:10:00 -0500

    changed size of HOS window

--- extra_code/scoreFunction.py
+++ extra_code/scoreFunction.py
@@ -69,6 +69,7 @@ def scoreAS(block,upgradeInv,multiplier,score):
 def HOS():
     hos = tk.Tk()
     hos.title("Hidden Ore Values:")
+    hos.geometry('500x500')
 
     text = tk.Label(hos, text='Potone = -1\nResin = 12.5\nPoisonous Potato = 537.25')
     text.pack(pady=5,padx=5)
