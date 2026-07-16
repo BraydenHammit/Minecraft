c7a9b1347ba9c727dd13360cb255e0ec04baca6b
Sheldon1436 <bh547516@georgetownisd.org>
2026-07-16 14:56:00 -0500

    changed var name

--- extra_code/toolTips.py
+++ extra_code/toolTips.py
@@ -4,24 +4,24 @@ class toolTip:
     def __init__(self, button, text):
         self.button = button
         self.text = text
-        self.tip_window = None
+        self.tooltip = None
         
         self.button.bind("<Enter>", self.show)
         self.button.bind("<Leave>", self.hide)
 
     def show(self, event=None):
-        if self.tip_window or not self.text:
+        if self.tooltip or not self.text:
             return
         x = self.button.winfo_rootx() + 20
         y = self.button.winfo_rooty() + self.button.winfo_height() + 5
-        self.tip_window = tk.Toplevel(self.button)
-        self.tip_window.wm_overrideredirect(True)
-        self.tip_window.wm_geometry(f"+{x}+{y}")
+        self.tooltip = tk.Toplevel(self.button)
+        self.tooltip.wm_overrideredirect(True)
+        self.tooltip.wm_geometry(f"+{x}+{y}")
         
-        label = tk.Label(self.tip_window, text=self.text, justify=tk.LEFT, background="#4A4A4A", relief=tk.SOLID, borderwidth=1, font=("ariel", "9", "normal"))
+        label = tk.Label(self.tooltip, text=self.text, justify=tk.LEFT, background="#4A4A4A", relief=tk.SOLID, borderwidth=1, font=("ariel", "9", "normal"))
         label.pack(ipadx=4, ipady=2)
 
     def hide(self, event=None):
-        if self.tip_window:
-            self.tip_window.destroy()
-            self.tip_window = None
+        if self.tooltip:
+            self.tooltip.destroy()
+            self.tooltip = None
